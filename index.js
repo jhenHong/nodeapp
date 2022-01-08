47dd31383985f5899ab42a0d48b97068f86be5b9
jane <mayday1721@gmail.com>
2022-01-08 13:43:47 +0800

    update

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 8080;
+const host = process.env['HOSTNAME'];
+
+app.get('/', (req, res) => {
+  res.send('ok')
+})
 
 app.get('/v1/*', (req, res) => {
-  res.send('Hello wold!' + req.url + '\n')
+  res.send('['+ host+ '] Hello wold!' + req.url + '\n')
 })
 
 app.get('/v2/*', (req, res) => {
-  res.send('Hello wold 2! ' + req.url + '\n')
+  res.send('['+ host +']Hello wold 2! ' + req.url + '\n')
 })
 
 
